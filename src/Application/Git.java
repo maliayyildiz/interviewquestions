1745ad1f7555940c25a3ac9518f6aba2cebd13db
maliayyildiz <maliayyildiz61@gmail.com>
2020-11-13 01:36:55 +0100

    selam 2

--- src/Application/Git.java
+++ src/Application/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("selam");
         System.out.println("selam");
+        System.out.println("2.selam");
     }
 }
